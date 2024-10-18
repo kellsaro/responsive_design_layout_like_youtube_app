abf3c40a3b8bc79aaabcc20584befdc2fd56d4e3
Maykell Sánchez Romero <kellsaro@gmail.com>
2024-10-18 11:44:20 -0500

    Add general padding

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -10,53 +10,56 @@ class DesktopBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('D E S K T O P'),
       ),
-      body: Row(
-        children: [
-          // first column
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            // first column
 
-          Expanded(
-            child: Column(
-              children: [
-                // video
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: AspectRatio(
-                    aspectRatio: 16 / 9,
-                    child: Container(
-                      height: 250,
-                      color: Colors.deepPurple[400],
+            Expanded(
+              child: Column(
+                children: [
+                  // video
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AspectRatio(
+                      aspectRatio: 16 / 9,
+                      child: Container(
+                        height: 250,
+                        color: Colors.deepPurple[400],
+                      ),
                     ),
                   ),
-                ),
 
-                // comment section
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: 8,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          color: Colors.deepPurple[300],
-                          height: 120,
-                        ),
-                      );
-                    },
+                  // comment section
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 8,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            color: Colors.deepPurple[300],
+                            height: 120,
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
-          // recommended videos
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 200,
-              color: Colors.deepPurple[300],
+            // recommended videos
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 200,
+                color: Colors.deepPurple[300],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -10,36 +10,39 @@ class MobileBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('M O B I L E'),
       ),
-      body: Column(
-        children: [
-          // video
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Container(
-                height: 250,
-                color: Colors.deepPurple[400],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // video
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AspectRatio(
+                aspectRatio: 16 / 9,
+                child: Container(
+                  height: 250,
+                  color: Colors.deepPurple[400],
+                ),
               ),
             ),
-          ),
 
-          // comment section & recommended videos
-          Expanded(
-            child: ListView.builder(
-              itemCount: 8,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Colors.deepPurple[300],
-                    height: 120,
-                  ),
-                );
-              },
+            // comment section & recommended videos
+            Expanded(
+              child: ListView.builder(
+                itemCount: 8,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      color: Colors.deepPurple[300],
+                      height: 120,
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
