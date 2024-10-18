88791b6883464037345caa3cd87069ac542dbdec
Maykell Sánchez Romero <kellsaro@gmail.com>
2024-10-18 11:42:12 -0500

    Add second column for recommended videos in desktop layout

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -10,33 +10,50 @@ class DesktopBody extends StatelessWidget {
       appBar: AppBar(
         title: const Text('D E S K T O P'),
       ),
-      body: Column(
+      body: Row(
         children: [
-          // video
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: AspectRatio(
-              aspectRatio: 16 / 9,
-              child: Container(
-                height: 250,
-                color: Colors.deepPurple[400],
-              ),
+          // first column
+
+          Expanded(
+            child: Column(
+              children: [
+                // video
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: Container(
+                      height: 250,
+                      color: Colors.deepPurple[400],
+                    ),
+                  ),
+                ),
+
+                // comment section
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 8,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          color: Colors.deepPurple[300],
+                          height: 120,
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
 
-          // comment section & recommended videos
-          Expanded(
-            child: ListView.builder(
-              itemCount: 8,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    color: Colors.deepPurple[300],
-                    height: 120,
-                  ),
-                );
-              },
+          // recommended videos
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              width: 200,
+              color: Colors.deepPurple[300],
             ),
           ),
         ],
